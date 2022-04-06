eae41f2a371231fb4475d3914d3d043d27f5f54d
puckqook <puckqook@gmail.com>
2022-04-06 15:33:29 +0900

    main page

---
+++ lib/NavigationPage/MainPage/AppBarPage/alram_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Alram_Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+              color: Colors.black
+          ),
+          elevation: 0.0,
+          centerTitle: false,
+          title: Text(
+            'Test',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: _buildAlramPage());
+  }
+}
+
+class _buildAlramPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+
+          ],
+        )
+    );
+  }
+}
