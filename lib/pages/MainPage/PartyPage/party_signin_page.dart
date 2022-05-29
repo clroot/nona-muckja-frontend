fd3620def89dc9c8e13a1018b661f81cadee6ce7
puckqook <puckqook@gmail.com>
2022-05-29 17:40:10 +0900

    partypage 수정

--- lib/pages/MainPage/PartyPage/party_signin_page.dart
+++ lib/pages/MainPage/PartyPage/party_signin_page.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nonamukja/pages/MainPage/PartyPage/category_select.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+const List<int> _selectint = <int>[
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11
+];
 
 class PartySigninPage extends StatelessWidget {
   @override
@@ -10,15 +27,16 @@ class PartySigninPage extends StatelessWidget {
           centerTitle: true,
           title: Text(
             '파티 생성',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
           ),
           backgroundColor: Colors.white,
           actions: <Widget>[
             Center(
               child: Container(
                 padding: const EdgeInsets.only(right: 15),
-                child: Text(
-                  "완료",
+                child: Text("완료",
                   style: TextStyle(
                     color: Color.fromARGB(255, 127, 91, 255),
                     fontSize: 21,
@@ -32,92 +50,181 @@ class PartySigninPage extends StatelessWidget {
   }
 }
 
+
+
 class BuildPartySigninPage extends StatefulWidget {
+
   @override
   State<BuildPartySigninPage> createState() => _BuildPartySigninPageState();
 }
 
 class _BuildPartySigninPageState extends State<BuildPartySigninPage> {
   TimeOfDay? time = const TimeOfDay(hour: 12, minute: 12);
-
+  int _selectedint= 0;
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+          height: 216,
+          padding: const EdgeInsets.only(top: 6.0),
+          // The Bottom margin is provided to align the popup above the system navigation bar.
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          // Provide a background color for the popup.
+          color: CupertinoColors.systemBackground.resolveFrom(context),
+          // Use a SafeArea widget to avoid system overlaps.
+          child: SafeArea(
+            top: false,
+            child: child,
+          ),
+        ));
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          child: Form(
-              child: TextField(
-            style: TextStyle(height: 2.5),
-            decoration: InputDecoration(
-              labelText: '매장 링크',
+          mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+
+            Container(
+                //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+
+                width: MediaQuery.of(context).size.width * 0.95,
+                child: Form(
+
+                    child: TextField(
+                      style: TextStyle(height: 2.5),
+                      decoration: InputDecoration(
+
+                        labelText : '매장 링크',
+                      ),
+
+
+                    )
+                ),
             ),
-          )),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          child: Form(
-              child: TextField(
-            style: TextStyle(height: 2.5),
-            decoration: InputDecoration(
-              labelText: '파티 제목',
+            Container(
+              //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+              width: MediaQuery.of(context).size.width * 0.95,
+              child: Form(
+                  child: TextField(
+                    style: TextStyle(height: 2.5),
+                    decoration: InputDecoration(
+
+                      labelText : '파티 제목',
+                    ),
+
+
+                  )
+              ),
             ),
-          )),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          child: Form(
-              child: TextField(
-            style: TextStyle(height: 2.5),
-            decoration: InputDecoration(
-              labelText: '모임 위치',
+
+            Container(
+              //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+              width: MediaQuery.of(context).size.width * 0.95,
+              child: Form(
+                  child: TextField(
+                    style: TextStyle(height: 2.5),
+                    decoration: InputDecoration(
+                      labelText : '모임 위치',
+                    ),
+
+
+                  )
+              ),
             ),
-          )),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          child: Form(
-              child: TextField(
-            style: TextStyle(height: 2.5),
-            decoration: InputDecoration(
-              labelText: '파티 인원 수',
+            Container(
+              //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+              child: Form(
+                  child: ListTile(
+                    title: RichText(
+                        text: TextSpan(
+                          text: '인원 수 : ',
+                         style: DefaultTextStyle.of(context).style,
+                          children: <TextSpan>[
+                          TextSpan(text : _selectint[_selectedint].toString(),
+                          style: TextStyle(color:Color.fromARGB(255, 127, 91, 255), ),
+                             )
+                            ]
+                          ),
+                    ),
+
+                    //Text('인원 수 : '+_selectint[_selectedint].toString()),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    onTap: () => _showDialog(
+                      CupertinoPicker(
+                        itemExtent: 25,
+                        onSelectedItemChanged: (int selectedItem) {
+                          setState(() {
+                            _selectedint = selectedItem;
+                          });
+                        },
+                        children: List<Widget>.generate(_selectint.length, (int index) {
+                          return Center(
+                            child: Text(
+                              _selectint[index].toString(),
+                            ),
+                          );
+                        }),
+
+
+
+                      ),
+
+                    )
+                  ),
+              ),
             ),
-          )),
-        ),
-        Container(
-          child: Form(
-            child: ListTile(
-              title: const Text('카테고리 설정'),
-              trailing: Icon(Icons.arrow_forward_ios),
-            ),
-          ),
-        ),
-        Container(
-          child: Form(
-            child: ListTile(
-              //richtext
-              title: Text(
-                '모임 시간 : ' +
-                    '${time!.hour.toString()}:${time!.minute.toString()}',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 127, 91, 255),
+
+
+            Container(
+              //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+
+              child: Form(
+                child: ListTile(
+                  title: Text('카테고리 설정'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () => pushNewScreen(context, screen: CategorySelectPage(), withNavBar: false),
+
                 ),
               ),
-              trailing: Icon(Icons.arrow_forward_ios),
-              onTap: () async {
-                TimeOfDay? newTime =
-                    await showTimePicker(context: context, initialTime: time!);
-                if (newTime != null) {
-                  setState(() {
-                    time = newTime;
-                  });
-                }
-              },
             ),
-          ),
-        ),
-      ],
-    );
+            Container(
+              //padding: const EdgeInsets.only(top: 10,right: 5,left: 5,),
+              child: Form(
+                child: ListTile(
+
+                  title: RichText(
+                    text: TextSpan(
+                      text: '모임 시간 : ',
+                      style: DefaultTextStyle.of(context).style,
+                      children: <TextSpan>[
+                        TextSpan(text : '${time!.hour.toString()}:${time!.minute.toString()}',
+                          style: TextStyle(color:Color.fromARGB(255, 127, 91, 255), ),
+                        )
+                      ]
+                    ),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () async {
+                    TimeOfDay? newTime = await showTimePicker(context: context, initialTime: time!);
+                    if(newTime != null) {
+                      setState(() {
+                        time = newTime;
+                      }
+                      );
+
+
+
+                    }
+                  },
+
+                ),
+              ),
+            ),
+
+          ],
+        );
   }
 }
